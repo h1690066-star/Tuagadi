f0f2563c179eaf5fcaa9ea59414ee2f0fcf9009e
kh.sardor <h1690066@gmail.com>
2026-05-24 06:52:10 +0300

    Loyihaga yangi o'zgarishlar kiritildi

---
+++ js/blog.js
@@ -0,0 +1,6 @@
+var loading = document.querySelector(".loding") 
+var header = document.querySelector(".header")
+
+window.addEventListener("load" , function(){
+    loading.classList.add("hidden")
+});
